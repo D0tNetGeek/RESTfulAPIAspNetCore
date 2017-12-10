1a2bbf23ada89eb05ad529f94a07b5406f30b836
Vinnie Malvi <vinod.malvi@yahoo.co.uk>
2017-12-10 03:18:39 +0000

    Added context factory for db migration

---
+++ Library.API/Entities/LibraryContextFactory.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Configuration;
+
+namespace Library.API.Entities
+{
+    public class LibraryContextFactory : IDesignTimeDbContextFactory<LibraryContext>
+    {
+        public LibraryContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<LibraryContext>();
+
+            var connectionString = configuration.GetConnectionString("libraryDBConnectionString");
+
+            builder.UseSqlServer(connectionString);
+
+            // Stop client query evaluation
+            builder.ConfigureWarnings(w =>
+                w.Throw(RelationalEventId.QueryClientEvaluationWarning));
+
+            return new LibraryContext(builder.Options);
+        }
+    }
+}
